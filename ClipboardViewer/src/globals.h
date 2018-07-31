f8eaafd9e279de2360007d1d548c8ab5a5324e51
K D Hedger <kdhedger68713@gmail.com>
2018-07-31 14:11:42 +0100

    Updated addresses

--- ClipboardViewer/src/globals.h
+++ ClipboardViewer/src/globals.h
@@ -1,9 +1,22 @@
-//
-//Keith Hedger
-//Wed Apr 18 18:07:43 BST 2007-2013
-//
-//globals.h
-//
+/*
+ *
+ * ©K. D. Hedger. Tue 31 Jul 14:10:54 BST 2018 keithdhedger@gmail.com
+
+ * This file (globals.h) is part of ClipboardViewer.
+
+ * ClipboardViewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * ClipboardViewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ClipboardViewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <gtk/gtk.h>
 #include "internet.h"
--- ClipboardViewer/src/main.cpp
+++ ClipboardViewer/src/main.cpp
@@ -1,9 +1,22 @@
-//
-//Keith Hedger
-//Tue Jun 12 16:22:48 BST 2007-2013
-//
-//clipboardview.cpp
-//
+/*
+ *
+ * ©K. D. Hedger. Tue 31 Jul 14:11:05 BST 2018 keithdhedger@gmail.com
+
+ * This file (main.cpp) is part of ClipboardViewer.
+
+ * ClipboardViewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * ClipboardViewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ClipboardViewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdlib.h>
 #include <string.h>
